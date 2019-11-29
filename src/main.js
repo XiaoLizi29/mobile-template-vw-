dc852626b7fcbc3ac28d3a251d1ed4c9d81f2874
栗艳宁 <2397394268@qq.com>
2019-11-29 16:43:47 +0800

    移动端页面开发模板(rem版)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入amfe-flexible.js
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-action-sheet v-model="show" :actions="actions" @select="onSelect" />
+    <van-button plain type="info">朴素按钮</van-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ActionSheet, Toast, Button } from 'vant'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    [ActionSheet.name]: ActionSheet,
+    [Button.name]: Button,
+  },
+  data() {
+    return {
+      show: true,
+      actions: [
+        { name: '选项' },
+        { name: '选项' },
+        { name: '选项', subname: '描述信息' }
+      ]
+    }
+  },
+
+  methods: {
+    onSelect(item) {
+      // 默认情况下，点击选项时不会自动关闭菜单
+      // 可以通过 close-on-click-action 属性开启自动关闭
+      this.show = false
+      Toast(item.name)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
